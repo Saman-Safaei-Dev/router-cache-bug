bdfc88533177fb686724b61af679bcad6cd82237
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-05-23 12:23:21 +0330

    fix: remove console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ import authentication from "@/middlewares/authentication";
 const middlewares = [authentication];
 
 function middleware(request: NextRequest) {
-  console.log("run")
   for (const middleware of middlewares) {
     const response = middleware(request);
     if (response !== null) return response;
